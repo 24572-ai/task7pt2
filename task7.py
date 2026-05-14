dc6829b7e51560dc506cf5246ef93c955b05c73f
Daniel Gasson <24572@burnside.school.nz>
2026-05-15 11:29:58 +1200

    this code fetches all the data from fighters.db, store it in the results variable, then prints it

--- task7.py
+++ task7.py
@@ -1,2 +1,9 @@
-print ("Hello world!")
-print ("hello world!!")
+import sqlite3
+
+db = sqlite3.connect("fighters.db")
+cursor = db.cursor()
+sql = "select * from fighters;"
+cursor.execute(sql)
+results = cursor.fetchall()
+print (results)
+db.close()
