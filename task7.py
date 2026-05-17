4b2c6043058b6e3153838df61fcf2f8cd782d609
Daniel Gasson <24572@burnside.school.nz>
2026-05-18 10:11:05 +1200

    added a for loop to print results more organised, and added a docstring to explain print_all_aircraft fucntion

--- task7.py
+++ task7.py
@@ -7,12 +7,16 @@ DATABASE = "fighters.db"
 
 #functions
 def print_all_aircraft():
+    '''print all aircraft nicely'''
     db = sqlite3.connect(DATABASE)
     cursor = db.cursor()
     sql = "select * from fighters;"
     cursor.execute(sql)
     results = cursor.fetchall()
-    print (results)
+    #loop through all results
+    for fighter in results:
+        print (fighter)
+    #loop ends
     db.close()
 
 
